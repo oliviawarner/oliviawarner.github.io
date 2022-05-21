ca9ea3e250863fb054a5e2377fbf645e1749d8fa
Liv Warner <ojw1002@sru.edu>
2022-05-21 12:55:04 -0400

    added another home nav path

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -10,6 +10,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="home" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="portfolio" element={<Portfolio />} />
         <Route path="contact" element={<Contact />} />     
