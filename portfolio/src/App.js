a3664290040c591456f6e2fdbb2ef8989a8b5ded
Liv Warner <ojw1002@sru.edu>
2022-05-21 11:55:35 -0400

    added routing

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import About from './components/About.js';
+import Contact from './components/Contact.js';
+import Portfolio from './components/Portfolio.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Welcome!</h1>
+      <Routes>
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="portfolio" element={<Portfolio />} />
+      </Routes>
+    </>
   );
 }
 
