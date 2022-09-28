41f31cb21754db7bca4fa3b8302e3f2e769ca438
Liv Warner <ojw1002@sru.edu>
2022-09-28 00:19:03 -0400

    redid nav

--- portfolio/web/components/nav.js
+++ portfolio/web/components/nav.js
@@ -4,29 +4,23 @@ class Nav extends HTMLElement {
     }
         connectedCallback() {
             this.innerHTML = `
-            <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
-                <div class="container-fluid">
-                    <a class="navbar-brand fw-bold" href="#">
-                    Olivia Warner
-                    </a>
-                        <div class="collapse navbar-collapse" id="navBar">
-                            <ul class="navbar-nav">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">About</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="https://oliviawarner.hashnode.dev/">Blog</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Projects</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Contact</a>
-                                </li>
-                            </ul>
-                        </div>
-                </div>
-            </nav>
+            <div class="container sticky-top">
+            <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+              <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none font-monospace fs-5 fw-bold">
+              Olivia Warner
+              </a>
+        
+              <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="#" class="nav-link px-2 link-dark">About</a></li>
+                <li><a href="#" class="nav-link px-2 link-dark">Work</a></li>
+                <li><a href="#" class="nav-link px-2 link-dark">Contact</a></li>
+              </ul>
+        
+              <div class="col-md-3 text-end">
+                <button type="button" class="btn btn-light" id="blog_btn">Blog</button>
+              </div>
+            </header>
+          </div>
             `;
     }
 }
