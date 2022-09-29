2a47c0d416a1db89446dfa8625d58448051a7a98
Liv Warner <ojw1002@sru.edu>
2022-09-28 22:02:36 -0400

    added links and resume download

--- portfolio/web/components/nav.js
+++ portfolio/web/components/nav.js
@@ -7,14 +7,14 @@ class Nav extends HTMLElement {
             <div class="container sticky-top">
             <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
               
-              <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none font-monospace fs-5 fw-bold">
+              <a href="#about" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none font-monospace fs-5 fw-bold">
                 Olivia Warner
               </a>
         
               <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="#" class="nav-link px-2 link-dark">About</a></li>
-                <li><a href="#" class="nav-link px-2 link-dark">Work</a></li>
-                <li><a href="#" class="nav-link px-2 link-dark">Contact</a></li>
+                <li><a href="#about" class="nav-link px-2 link-dark">About</a></li>
+                <li><a href="#work" class="nav-link px-2 link-dark">Work</a></li>
+                <li><a href="#contact" class="nav-link px-2 link-dark">Contact</a></li>
               </ul>
         
               <div class="col-md-3 text-end">
@@ -27,3 +27,11 @@ class Nav extends HTMLElement {
     }
 }
 customElements.define('nav-component', Nav);
+
+$(document).ready(function() {
+  //Hashnode Blog
+  $('#nav_blog_btn').click(function () {
+    window.open('https://oliviawarner.hashnode.dev/', '_blank');
+  });
+
+});
---
+++ portfolio/web/components/shared.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    //Github
+    $('.github').click(function () {
+      window.open('https://github.com/oliviawarner', '_blank');
+    });
+
+    //Linkedin
+    $('.linkedin').click(function () {
+        window.open('https://www.linkedin.com/in/olivia-warner-a32015151/', '_blank');
+    });
+
+    //Hashnode
+    $('.hashnode').click(function () {
+        window.open('https://oliviawarner.hashnode.dev/', '_blank');
+    });  
+    
+});
