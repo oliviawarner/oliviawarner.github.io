25501a2e64b96e40dea0e0e0c9656720feb3e737
Liv Warner <ojw1002@sru.edu>
2022-09-28 22:42:23 -0400

    added copywright and arrow down

--- portfolio/web/components/about.js
+++ portfolio/web/components/about.js
@@ -12,7 +12,7 @@ class About extends HTMLElement {
               </div>
               <div class="col-lg-6">
                 <h1 class="display-5 fw-bold lh-1 mb-3">Hello World,</h1>
-                <p class="lead">I am a developer and designer from Pittsburgh, Pennsylvania.</p>
+                <p class="lead">I am a software developer from Pittsburgh, Pennsylvania who creates digital experiences for the web.</p>
 
                 <!--Socials-->
                 <div class="d-grid gap-2 d-md-flex justify-content-md-start">
@@ -26,6 +26,7 @@ class About extends HTMLElement {
                   <!--Hashnode-->
                   <svg class="hashnode" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M35.19 171.1C-11.72 217.1-11.72 294 35.19 340.9L171.1 476.8C217.1 523.7 294 523.7 340.9 476.8L476.8 340.9C523.7 294 523.7 217.1 476.8 171.1L340.9 35.19C294-11.72 217.1-11.72 171.1 35.19L35.19 171.1zM315.5 315.5C282.6 348.3 229.4 348.3 196.6 315.5C163.7 282.6 163.7 229.4 196.6 196.6C229.4 163.7 282.6 163.7 315.5 196.6C348.3 229.4 348.3 282.6 315.5 315.5z"/></svg>
                 </div>
+                <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_vfmyxu76.json"  background="transparent"  speed="1"  style="width: 150px; height: 150px;"  loop  autoplay id="arrow_down"></lottie-player>
               </div>
             </div>
           </div>
@@ -40,6 +41,11 @@ $(document).ready(function() {
   //Resume Download
   $('#resume_download').click(function() {
       window.open('../files/Olivia_Warner_Resume_2022.pdf', '_blank');
+  });
+
+  //Arrow Down
+  $('#arrow_down').click(function () {
+    window.location.href="#work";
   });
 
 });
