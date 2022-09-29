d269d6b170fb9013343eb7052d572bc39f2f7e8d
Liv Warner <ojw1002@sru.edu>
2022-09-29 18:28:35 -0400

    added contact email plug in

--- portfolio/web/components/contact.js
+++ portfolio/web/components/contact.js
@@ -8,17 +8,18 @@ class Contact extends HTMLElement {
             <div class="row align-items-center g-lg-5 py-5">
                 <h1 class="display-4 fw-bold lh-1 mb-3" style="text-align:center">Contact Me</h1>
               <div class="col-lg-7 mx-auto col-lg-7">
-                <form class="p-4 p-md-5 border rounded-3 bg-light">
+                <form class="p-4 p-md-5 border rounded-3 bg-light" id="contact-form">
+                <input type="hidden" class="form-control" name="contact_number">
                 <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="floatingName" placeholder="Name">
+                <input type="text" class="form-control" id="floatingName" placeholder="Name" name="user_name">
                 <label for="floatingName">Name</label>
                 </div>
                   <div class="form-floating mb-3">
-                    <input type="email" class="form-control" id="floatingEmail" placeholder="Email">
+                    <input type="email" class="form-control" id="floatingEmail" placeholder="Email" name="user_email">
                     <label for="floatingEmail">Email Address</label>
                   </div>
                   <div class="form-floating mb-3">
-                    <textarea class="form-control" id="floatingMessage" rows="4" placeholder="Message" style="height:100%"></textarea>
+                    <textarea class="form-control" id="floatingMessage" rows="4" placeholder="Message" style="height:100%" name="user_message"></textarea>
                     <label for="floatingMessage">Message</label>
                   </div>
                   <button class="w-100 btn btn-lg btn-warning" type="submit" id="send_message_btn">Send Message</button>
@@ -32,3 +33,20 @@ class Contact extends HTMLElement {
         }
     }
     customElements.define('contact-component', Contact);
+
+$(document).ready(function() {
+        emailjs.init('TESEuE_vHOK9dI3xH');
+        document.getElementById('contact-form').addEventListener('submit', function(event) {
+            event.preventDefault();
+            // generate a five digit number for the contact_number variable
+            this.contact_number.value = Math.random() * 100000 | 0;
+            // these IDs from the previous steps
+            emailjs.sendForm('service_wedvjoh', 'template_f4sdf1v', this)
+                .then(function() {
+                    console.log('SUCCESS!');
+                }, function(error) {
+                    console.log('FAILED...', error);
+                });
+        });
+    
+    });
