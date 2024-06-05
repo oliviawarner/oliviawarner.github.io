03141ab0a33fe6a65777f9b66a2c451c80c8cdf5
livwarner <ojw1002@sru.edu>
2024-06-05 09:20:21 -0400

    fixed home nav page reference

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -129,7 +129,7 @@ function MobileNavigation(props) {
             </div>
             <nav className="mt-6">
               <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
-                <MobileNavItem href="/home">home</MobileNavItem>
+                <MobileNavItem href="/">home</MobileNavItem>
                 <MobileNavItem href="/projects">projects</MobileNavItem>
                 <MobileNavItem href="/contact">contact</MobileNavItem>
               </ul>
@@ -168,7 +168,7 @@ function DesktopNavigation(props) {
   return (
     <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-        <NavItem href="/home">home</NavItem>
+        <NavItem href="/">home</NavItem>
         <NavItem href="/projects">projects</NavItem>
         <NavItem href="/contact">contact</NavItem>
       </ul>
