9cbb01efb34062228277792de2358e6ddadf3582
livwarner <ojw1002@sru.edu>
2024-06-05 09:27:49 -0400

    updated projects file

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -10,39 +10,25 @@ import logoPlanetaria from '@/images/logos/planetaria.svg'
 
 const projects = [
   {
-    name: 'Planetaria',
+    name: 'Intense Tennis Camps',
     description:
       'Creating technology to empower civilians to explore space on their own terms.',
-    link: { href: 'http://planetaria.tech', label: 'planetaria.tech' },
+    link: { href: 'http://www.intensetenniscamps.com', label: 'intensetenniscamps.com' },
     logo: logoPlanetaria,
   },
   {
-    name: 'Animaginary',
+    name: 'Android Task Manager',
     description:
       'High performance web animation library, hand-written in optimized WASM.',
     link: { href: '#', label: 'github.com' },
     logo: logoAnimaginary,
   },
   {
-    name: 'HelioStream',
+    name: 'Twitter Topic Modeling',
     description:
       'Real-time video streaming library, optimized for interstellar transmission.',
     link: { href: '#', label: 'github.com' },
     logo: logoHelioStream,
-  },
-  {
-    name: 'cosmOS',
-    description:
-      'The operating system that powers our Planetaria space shuttles.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoCosmos,
-  },
-  {
-    name: 'OpenShuttle',
-    description:
-      'The schematics for the first rocket I designed that successfully made it to orbit.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoOpenShuttle,
   },
 ]
 
@@ -58,7 +44,7 @@ function LinkIcon(props) {
 }
 
 export const metadata = {
-  title: 'Projects',
+  title: 'projects',
   description: 'Things I’ve made trying to put my dent in the universe.',
 }
 
