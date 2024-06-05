40967a1d8f54ca8b5c3152d24078f552d6f3d977
livwarner <ojw1002@sru.edu>
2024-06-05 19:55:58 -0400

    removed articles and unneeded logos

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -214,8 +214,6 @@ function Photos() {
 }
 
 export default async function Home() {
-  let articles = (await getAllArticles()).slice(0, 4)
-
   return (
     <>
       <Container className="mt-9">
@@ -244,11 +242,6 @@ export default async function Home() {
       <Photos />
       <Container className="mt-24 md:mt-28">
         <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
-          <div className="flex flex-col gap-16">
-            {articles.map((article) => (
-              <Article key={article.slug} article={article} />
-            ))}
-          </div>
           <div className="space-y-10 lg:pl-16 xl:pl-24">
             <Resume />
           </div>
