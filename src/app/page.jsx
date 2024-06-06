6e6eb2784662b102dcc877c03b925994b00ba2c1
livwarner <ojw1002@sru.edu>
2024-06-05 22:36:42 -0400

    added photo animation

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,9 @@
+// components/Photos.jsx
+"use client"; // Ensure this directive is at the top of the file
 import Image from 'next/image'
 import Link from 'next/link'
 import clsx from 'clsx'
-
+import {motion} from 'framer-motion'
 import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
 import {
@@ -211,17 +213,18 @@ function Certifications() {
 }
 
 function Photos() {
-  let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
+  const rotations = ['rotate-2', '-rotate-2', 'rotate-2', '-rotate-2', 'rotate-2'];
 
   return (
     <div className="mt-16 sm:mt-20">
       <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
         {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
-          <div
+          <motion.div
             key={image.src}
+            whileHover={{ scale: 1.05, boxShadow: "0px 10px 30px rgba(0, 0, 0, 0.12)" }}
             className={clsx(
               'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800',
-              rotations[imageIndex % rotations.length],
+              rotations[imageIndex % rotations.length]
             )}
           >
             <Image
@@ -230,14 +233,14 @@ function Photos() {
               sizes="(min-width: 640px) 18rem, 11rem"
               className="absolute inset-0 h-full w-full object-cover"
             />
-          </div>
+          </motion.div>
         ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default async function Home() {
+export default function Home() {
   return (
     <>
       <Container className="mt-9">
