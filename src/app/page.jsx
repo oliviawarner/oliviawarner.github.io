3865a0e8efdb2a767ba3195628fced27127bd593
livwarner <ojw1002@sru.edu>
2024-06-05 23:24:07 -0400

    changed work to experience

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -154,7 +154,7 @@ function Resume() {
 <div className="rounded-2xl border border-zinc-300 p-6 dark:border-zinc-500/50">
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
         <BriefcaseIcon className="h-6 w-6 flex-none" />
-        <span className="ml-3">Work</span>
+        <span className="ml-3">Experience</span>
       </h2>
       <ol className="mt-6 space-y-4">
         {resume.map((role, roleIndex) => (
