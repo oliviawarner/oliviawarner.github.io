e43ba19862da1b58b63a136cb2c48360ef11fab8
livwarner <ojw1002@sru.edu>
2024-06-06 13:22:45 -0400

    updated deployment scripts

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
-import rehypePrism from '@mapbox/rehype-prism'
-import nextMDX from '@next/mdx'
-import remarkGfm from 'remark-gfm'
+import rehypePrism from '@mapbox/rehype-prism';
+import nextMDX from '@next/mdx';
+import remarkGfm from 'remark-gfm';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-}
+  trailingSlash: true,
+  output: 'export',
+};
 
 const withMDX = nextMDX({
   extension: /\.mdx?$/,
@@ -13,6 +15,6 @@ const withMDX = nextMDX({
     remarkPlugins: [remarkGfm],
     rehypePlugins: [rehypePrism],
   },
-})
+});
 
-export default withMDX(nextConfig)
+export default withMDX(nextConfig);
