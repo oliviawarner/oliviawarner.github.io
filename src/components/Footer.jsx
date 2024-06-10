d7ff58ce54516248313528092fed45d4061fe815
livwarner <ojw1002@sru.edu>
2024-06-09 21:56:11 -0400

    fix broken footer link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,7 +20,7 @@ export function Footer() {
           <ContainerInner>
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
               <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
-                <NavLink href="/home">home</NavLink>
+                <NavLink href="/">home</NavLink>
                 <NavLink href="/projects">projects</NavLink>
                 <NavLink href="/contact">contact</NavLink>
               </div>
